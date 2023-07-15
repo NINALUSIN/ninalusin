cbcf93da3d2bcb12aef338f29ed5f3c6091ea38a
ninalusin <nina.lusin.2502@gmail.coms>
2023-07-15 16:48:32 +0200

    Javascript sample site changes.

--- public/assets/js/dashboards-javascript.js
+++ public/assets/js/dashboards-javascript.js
@@ -4,7 +4,28 @@ $(document).ready(function () {
 
 document.querySelector('#javascript-alert-button').addEventListener('click', () => alert('Obvestilo prikazano :)'));
 
-document.querySelector('#characterCounter').oninput = function () {
+//Character Counter
+document.querySelector('#characterCounter').addEventListener('input', characterCounter);
+
+//Sum Calculator
+document.querySelector('#sumFirstNumber').addEventListener('input', sumCalculator);
+document.querySelector('#sumSecondNumber').addEventListener('input', sumCalculator);
+
+function characterCounter() {
   document.querySelector('#characterCounterShow').innerHTML =
     'Število znakov: ' + document.querySelector('#characterCounter').value.length;
-};
+
+  document.querySelector('#firstLastCharacter').innerHTML =
+    'Prvi znak: ' +
+    document.querySelector('#characterCounter').value[0] +
+    '<br>' +
+    'Zadnji znak: ' +
+    document.querySelector('#characterCounter').value[document.querySelector('#characterCounter').value.length - 1];
+}
+
+function sumCalculator() {
+  let firstNumber = document.querySelector('#sumFirstNumber').value;
+  let secondNumber = document.querySelector('#sumSecondNumber').value;
+
+  document.querySelector('#sumResult').value = Number(firstNumber) + Number(secondNumber);
+}
--- resources/views/content/javascript.blade.php
+++ resources/views/content/javascript.blade.php
@@ -32,6 +32,7 @@ $configData = Helper::appClasses();
       <div class="d-flex align-items-end row">
         <div class="col-md-12 order-2 order-md-1">
           <div class="card-body">
+          <h4 class="mb-0">Alert</h4> <br>
 
           <button type="button" class="btn btn-primary" id="javascript-alert-button">Prikaži obvestilo</button>
 
@@ -49,15 +50,22 @@ $configData = Helper::appClasses();
       <div class="d-flex align-items-end row">
         <div class="col-md-12 order-2 order-md-1">
           <div class="card-body">
+            <h4 class="mb-0">Števec znakov in prvi/zadnji znak</h4> <br>
 
             <div class="form-floating form-floating-outline">
               <input type="text" class="form-control" id="characterCounter" placeholder="Vnesite besedilo" aria-describedby="floatingInputHelp" />
-              <label for="characterCounter">Števec znakov</label>
+              <label for="characterCounter">Besedilo</label>
               <div id="characterCounter" class="form-text">Vnesite besedilo</div>
             </div>
 
             <div class="alert alert-outline-success" role="alert" id="characterCounterShow">
               Število znakov: 0
+            </div>
+
+            <div class="alert alert-outline-warning" role="alert" id="firstLastCharacter">
+              Prvi znak: /
+              <br>
+              Zadnji znak: /
             </div>
 
           </div>
@@ -74,8 +82,41 @@ $configData = Helper::appClasses();
       <div class="d-flex align-items-end row">
         <div class="col-md-12 order-2 order-md-1">
           <div class="card-body">
+            <div class="row g-4">
+              <h4 class="mb-0">Kalkulator - seštevanje</h4> <br>
 
-            TEST
+              <div class="col-md-2">
+                <div class="form-floating form-floating-outline">
+                  <input type="number" class="form-control" id="sumFirstNumber" placeholder="Vnesite število" aria-describedby="floatingInputHelp" />
+                  <label for="sumFirstNumber">Prvo število</label>
+                  <div id="sumFirstNumber" class="form-text">Vnesite številko</div>
+                </div>
+              </div>
+              <div class="col-md-1">
+                +
+              </div>
+              <div class="col-md-2">
+                <div class="form-floating form-floating-outline">
+                  <input type="number" class="form-control" id="sumSecondNumber" placeholder="Vnesite število" aria-describedby="floatingInputHelp" />
+                  <label for="sumSecondNumber">Drugo število</label>
+                  <div id="sumSecondNumber" class="form-text">Vnesite število</div>
+                </div>
+              </div>
+              <div class="col-md-1">
+                =
+              </div>
+              <div class="col-md-2">
+                <div class="form-floating form-floating-outline">
+                  <input type="text" class="form-control" id="sumResult"  disabled />
+
+                </div>
+              </div>
+            </div>
+
+
+
+
+
 
           </div>
 
@@ -85,6 +126,7 @@ $configData = Helper::appClasses();
       </div>
     </div>
   </div>
+
   <!--/ Gamification Card -->
 
  <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>.
